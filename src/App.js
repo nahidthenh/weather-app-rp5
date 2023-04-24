07db08160431a23262702887ae5faef9007002cb
nahidthenh <nhgroup18@gmail.com>
2023-04-24 22:00:11 +0600

    Daynamic Search

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 const App = () => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?id=dhaka&appid=9fc7aa67b16b24ad22623966ce5e9a5e`
-  console.log(url);
+
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=9fc7aa67b16b24ad22623966ce5e9a5e`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      setLocation('')
+    }
+  }
+
   return (
     <div className='weather-app-wrap'>
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter location...'
+          type="text"
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Dhaka</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>60°F</h1>
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
           <div className="description">
             <p>Clear</p>
