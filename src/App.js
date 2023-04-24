33f0499ffedc37a34b47fa0b448bafdd1c46ba6e
nahidthenh <nhgroup18@gmail.com>
2023-04-24 20:56:10 +0600

    Frontend Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import axios from 'axios';
 const App = () => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?id=dhaka&appid=9fc7aa67b16b24ad22623966ce5e9a5e`
+  console.log(url);
   return (
-    <div>
-      <h2>Header Components</h2>
+    <div className='weather-app-wrap'>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dhaka</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Clear</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className='bold'>60°F</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className='bold'>20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className='bold'>12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
