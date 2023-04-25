e98c500c626bd9663621ae723434f6dcd4453db2
nahidthenh <nhgroup18@gmail.com>
2023-04-25 12:48:08 +0600

    real data added

--- src/App.js
+++ src/App.js
@@ -37,23 +37,25 @@ const App = () => {
             {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
           <div className="description">
-            <p>Clear</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
-        <div className="bottom">
-          <div className="feels">
-            <p className='bold'>60°F</p>
-            <p>Feels Like</p>
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-          <div className="humidity">
-            <p className='bold'>20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <p className='bold'>12 MPH</p>
-            <p>Wind Speed</p>
-          </div>
-        </div>
+        }
       </div>
     </div>
   );
